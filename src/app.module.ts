6bd0e99a7cdb09375d9f838dd0840a6aa2780192
Simran <simran.develop@gmail.com>
2025-07-17 17:56:54 +1200

    feat: replace PrismaModule with DatabaseModule and implement DatabaseService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [DatabaseModule],
   controllers: [],
   providers: [],
 })
